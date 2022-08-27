f886bfe7c5badc53e68e7a0aad76fd9799f6fecd
richardklapwijk <110915049+richardklapwijk@users.noreply.github.com>
2022-08-27 21:10:06 +0200

    main.blocks, main.ts bijwerken

--- main.ts
+++ main.ts
@@ -2,6 +2,6 @@ input.onButtonPressed(Button.A, function () {
     media.sendCode(media.keys(media._MediaKey.playPause))
 })
 input.onButtonPressed(Button.B, function () {
-    media.sendCode(media.keys(media._MediaKey.previous))
+    media.sendCode(media.keys(media._MediaKey.next))
 })
 media.startMediaService()
